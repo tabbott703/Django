d9bea0997152f60459d4d93113bada8ebf2b61a5
tabbott703 <tabbott3@charlotte.edu>
2024-09-22 18:42:38 -0400

    Activity feed

--- project1/base/views.py
+++ project1/base/views.py
@@ -67,16 +67,20 @@ def registerPage(request):
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+
     rooms = Room.objects.filter(
-        Q(topic__name__icontains = q) |
+        Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
-    ) 
+    )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q))[0:3]
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
