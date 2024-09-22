a704e4a469d3621fb39fa1516a07e6bcbfd8cd77
tabbott703 <tabbott3@charlotte.edu>
2024-09-22 19:31:23 -0400

    Static files

--- project1/base/urls.py
+++ project1/base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
     path('register/', views.registerPage, name="register"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     ]
